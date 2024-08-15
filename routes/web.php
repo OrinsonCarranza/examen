6b129930bcaf5d10ce018079c7b0b511e8a0a77d
OrinsonCarranza <orinsoncarranza@gmail.com>
2024-08-15 13:12:14 -0500

    Examen Final

--- routes/web.php
+++ routes/web.php
@@ -3,30 +3,25 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClienteController;
 
-// Ruta para Home
 Route::get('/', function () {
     return view('home', ['message' => 'Estás en el Inicio']);
 })->name('home');
 
-// Ruta para Servicios
 Route::get('/servicios', function () {
     return view('layouts.aplicacion.servicios', ['message' => 'Estás en Servicios. Ofrecemos distintos servicios']);
 })->name('servicios');
 
-// Ruta para Proyectos
 Route::get('/proyectos', function () {
     return view('layouts.aplicacion.proyectos', ['message' => 'Estás en Proyectos. Los mejores Proyectos']);
 })->name('proyectos');
 
-// Rutas para Clientes utilizando Resource Controller
+//Controlador para todas las opciones de cliente
 Route::resource('clientes', ClienteController::class);
 
-// Ruta para Blog
 Route::get('/blog', function () {
     return view('layouts.aplicacion.blog', ['message' => 'Estás en el Blog. Escribe algo']);
 })->name('blog');
 
-// Ruta para Contacto
 Route::get('/contacto', function () {
     return view('layouts.aplicacion.contacto', ['message' => 'Estás en Contacto']);
 })->name('contacto');
